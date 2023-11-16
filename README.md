93d5518e9e35865ac1f17014ffb7ac1e3ce46aae
Jona Ohme <ohmejona@gmail.com>
2023-11-16 23:41:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1 align="center">.dotfiles</h1>
+My configuration files. Mostly copied from others and then refined for my needs.
+
+## Clone Repository
+```console
+$ git clone git@github.com:ohnchen/.dotfiles.git
+```
+## Author
+
+**Jona Ohme**
+
+- [Profile](https://github.com/ohnchen "ohnchen")
+
+## 🤝 Support
+
+Contributions, issues, and feature requests are welcome!
+
+Give a ⭐️ if you like this project!
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# expects everything to be installed:
+# nvim, tmux, alacritty
+#
+#
+# clone nvim-fork repo into .dotfiles
+# does not need a link because it is its own repo
+git clone git@github.com:ohnchen/nvim-config.git "$HOME"/.config/nvim
+
+# i3 setup
+ln -sf i3/config "$HOME"/.config/i3/config 
+ln -sf i3status/config "$HOME"/.config/i3status/config 
+
+# alacritty setup
+ln -sf alacritty/alacritty.yml "$HOME"/.config/alacritty/alacritty.yml 
+
+#tmux setup
+if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
+  printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
+ at default location: \$HOME/.tmux/plugins/tpm.\n"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+ln -sf tmux/tmux.conf "$HOME"/.tmux.conf;
+
