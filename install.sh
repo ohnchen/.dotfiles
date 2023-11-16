74b6212935c75765772b0921347859e354b4bae0
Jona Ohme <ohmejona@gmail.com>
2023-11-16 23:57:49 +0100

    adjusted fontsize for alacritty

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@
 #
 # clone nvim-fork repo into .dotfiles
 # does not need a link because it is its own repo
+
 git clone git@github.com:ohnchen/nvim-config.git "$HOME"/.config/nvim
 
 # i3 setup
-ln -sf ./i3/config "$HOME"/.config/i3/config 
-ln -sf ./i3status/config "$HOME"/.config/i3status/config 
+ln -sf "$PWD"/i3/config "$HOME"/.config/i3/config 
+ln -sf "$PWD"/i3status/config "$HOME"/.config/i3status/config 
 
 # alacritty setup
-ln -sf ./alacritty/alacritty.yml "$HOME"/.config/alacritty/alacritty.yml 
+ln -sf "$PWD"/alacritty/alacritty.yml "$HOME"/.config/alacritty/alacritty.yml 
 
 #tmux setup
 if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
@@ -22,5 +23,5 @@ if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-ln -sf ./tmux/tmux.conf "$HOME"/.tmux.conf
+ln -sf "$PWD"/tmux/tmux.conf "$HOME"/.tmux.conf
 
